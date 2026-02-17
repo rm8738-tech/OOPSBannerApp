b26d42056e9276ee756ab88e1eae343b91ec49bc
Rishyanth <rm8738@srmist.edu.in>
2026-02-17 09:42:25 +0530

    Remove conflict markers

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,5 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("OOPS");
-=======
 
         System.out.println(" OOO    OOO    PPPP   SSSS ");
         System.out.println("O   O  O   O   P   P  S     ");
@@ -10,6 +7,5 @@ public class OOPSBannerApp {
         System.out.println("O   O  O   O   P          S ");
         System.out.println(" OOO    OOO    P      SSSS  ");
 
->>>>>>> feature/UC2-BannerPrintStatements
     }
 }
