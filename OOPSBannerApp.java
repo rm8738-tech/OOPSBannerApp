d37bed7852e3e88de697ce34ce5229366e82e2af
Rishyanth <rm8738@srmist.edu.in>
2026-02-17 10:27:13 +0530

    UC2: banner using string concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,11 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println(" OOO    OOO    PPPP   SSSS ");
-        System.out.println("O   O  O   O   P   P  S     ");
-        System.out.println("O   O  O   O   PPPP    SSS  ");
-        System.out.println("O   O  O   O   P          S ");
-        System.out.println(" OOO    OOO    P      SSSS  ");
+        System.out.println(" ***  " + " ***  " + "****  " + " **** ");
+        System.out.println("*   * " + "*   * " + "*   * " + "*     ");
+        System.out.println("*   * " + "*   * " + "****  " + " ***  ");
+        System.out.println("*   * " + "*   * " + "*     " + "    * ");
+        System.out.println(" ***  " + " ***  " + "*     " + " **** ");
 
     }
 }
